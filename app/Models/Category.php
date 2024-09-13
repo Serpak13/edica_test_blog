b7c3c65e217b2a59f1feb200116b89860505b2f1
serpak13 <susafonov@yandex.ru>
2024-09-14 00:18:33 +0500

    add

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'categories'; //явно указываем  к какой таблице привязана данная модель
+    protected $guarded = false; //означает что все поля модели разрешены для массового заполнения
+
 }
